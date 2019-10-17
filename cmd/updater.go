ee944dcd515027eb9a8b6026638167847530887b
Jordan Gregory <jordangregory@granular.ag>
2019-10-17 13:52:51 -0500

    bump version

--- cmd/updater.go
+++ cmd/updater.go
@@ -18,7 +18,7 @@ var (
 	// CFupdaterCmd is the main command of cfupdater
 	CFupdaterCmd = &cobra.Command{
 		Use:     "cfupdater",
-		Version: "0.2.1",
+		Version: "0.2.2",
 		Short:   "cfupdater is an app to update cloudflare DNS resouce records with the contents of ipinfo.io",
 		Run:     cfupdaterFunc,
 	}
